a1a9ed4ca2deb5ad1495cd4d43544c379c18f094
hssainazaqy <hssain.azaqy@campus.technion.ac.il>
2022-05-08 09:37:51 +0300

    update 1 - card.cpp added and almost done

---
+++ Card.cpp
@@ -0,0 +1,86 @@
+
+#ifndef EX2_CARD_CPP
+#define EX2_CARD_CPP
+#include "Card.h"
+
+
+
+//----------------------constructor--------------------------
+Card Card(CardType type, const CardStats& stats){
+    m_effects = type;
+    m_stats = stats;
+}
+//----------------------ApllyEncounter-----------------------
+void applyEncounter(Player& player) const{
+    if(m_effects == CardType::Battle){
+        bool win = true;
+        if(player.getAttackStrength()>=m_stats->force){
+            printBattleResult(win);
+            player.levelUp();
+            player.loot(m_stats->loot);
+        }
+        else{
+            player.damage(m_stats->hpLossOnDefeat);
+            win = false;
+            printBattleResult(win);
+            if(player.isKcnockedOut()){
+                std::cout << "Player is knocked out";
+            }
+        }
+    }
+    if(m_effects == CardType::Buff){
+        if(player.pay(m_stats->cost)){
+            player.buff(m_stats->buff);
+        }
+        else{
+            std::cout << "Not enough coins";
+        }
+    }
+    if(m_effects == CardType::Heal){
+        if(player.pay(m_stats->cost)){
+            player.heal(m_stats->heal);
+        }
+        else{
+            std::cout << "Not enough coins";
+        }
+    }
+    if(m_effects == CardType::Treasure){
+        player.addCoins(m_stats->loot);
+    }
+}
+//----------------------PrintCardInfo------------------------
+    void printInfo() const {
+        if (m_effects == CardType::Battle){
+            printBattleCardInfo(m_stats);
+        }
+
+        if (m_effects == CardType::Buff){
+            printBuffCardInfo(m_stats);
+        }
+
+        if (m_effects == CardType::Heal){
+            printHealCardInfo(m_stats);
+        }
+
+        if (m_effects == CardType::Treasure){
+            printTreasueCardInfo(m_stats);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
